5c54585956384de4ba0f3e9436ec1766b403ba58
spungbub <vlads1999@inbox.lv>
2022-10-10 18:45:50 +0300

    Simplified CreateJsonObjectFromUri because of try-catch block and edited comments for better readability.

--- OpenDataViewer/Controllers/OpenDataController.cs
+++ OpenDataViewer/Controllers/OpenDataController.cs
@@ -7,27 +7,22 @@ namespace OpenDataViewer.Controllers
 {   
     public class OpenDataController : Controller
     {
-        public async Task<JObject?> CreateJsonObjectFromUri(string requestUri, int? limitAmount = null)
+        public async Task<JObject> CreateJsonObjectFromUri(string requestUri, int? returnedRecordLimit = null)
         {
             string uri;
-            JObject jObj;
 
-            if (limitAmount != null && !requestUri.Contains("&limit="))
-                uri = requestUri + $"&limit={limitAmount}";
+            // Specifying final URI.
+            if (returnedRecordLimit != null && !requestUri.Contains("&limit="))
+                uri = requestUri + $"&limit={returnedRecordLimit}";
             else 
                 uri = requestUri;
 
-            // Get JSON string from api and parse it into a Json Object
+            // Getting JSON string from api and parsing it into a Json Object.
             HttpClient client = new();
             HttpResponseMessage response = await client.GetAsync(uri);
-            if (!response.IsSuccessStatusCode)  // Check if response from api is successful
-                return null;
-
             string json = await response.Content.ReadAsStringAsync();
-            if (json == null || json == "")     // Exception handling for json string being empty or null
-                return null;
+            JObject jObj = JObject.Parse(json);
 
-            jObj = JObject.Parse(json);
             return jObj;
         }
 
@@ -35,27 +30,29 @@ namespace OpenDataViewer.Controllers
         {
             return View();
         }
+
         public async Task<IActionResult> Show()
         {
             string uri = "https://data.gov.lv/dati/lv/api/3/action/datastore_search?resource_id=58e7bbf1-c296-41c9-b45f-e2dd67fc9f1d";
 
             try
             {
-                JObject? jObj = await CreateJsonObjectFromUri(uri);     // Creating a JObject from uri.
-                int totalRecords = (int)jObj["result"]!["total"]!;      // Count of all the records in the selected dataset.
-                int recCount = jObj["result"]!["records"]!.Count();    // Count of records that are returned by api call.
+                JObject jObj = await CreateJsonObjectFromUri(uri);  // Creating a JsonObject from uri.
 
-                // NOTE: data.gov.lv API returns only 100 records in pre-selected dataset.
+                int totalRecords = (int)jObj["result"]!["total"]!;  // Number of all records in the selected dataset.
+                int recCount = jObj["result"]!["records"]!.Count(); // Number of records that are returned by api call.
+
+                // NOTE: data.gov.lv API returns only 100 records of pre-selected dataset (because of pagination).
                 // If there are more then 100 records in the pre-selected dataset,
-                // set 'limit' attribute in URI to total records in dataset.
+                // set 'limit' attribute in URI to total records in the dataset.
                 if (totalRecords > 100)
                 {
                     jObj = await CreateJsonObjectFromUri(uri, totalRecords);
                     recCount = totalRecords;
                 }
 
-                // Iterate through each datarow, deserialize it into
-                // 'RegObjStat' class object and add to list.
+                // Iterating through each dataset record, deserializing it into
+                // a 'RegObjStat' class object and adding it to a list.
                 var records = jObj["result"]!["records"]!;
                 List<RegObjStat> datasetRecords = new();
                 for (int i = 0; i < recCount; i++)
@@ -65,14 +62,14 @@ namespace OpenDataViewer.Controllers
                     datasetRecords.Add(regObjStat);
                 }
 
-                // Save column names in a List.
+                // Saving column names in a list.
                 var fields = jObj["result"]!["fields"]!;
                 List<string> columnNames = new();
                 foreach (var field in fields)
                     columnNames.Add(field["id"]!.ToString());
-                ViewBag.ColumnNames = columnNames;    // Pass column names to view for creating table header.
+                ViewBag.ColumnNames = columnNames;    // Passing column names to view for creating table header.
 
-                // Pass a list of 'RegObjStat' type objects to view.
+                // Passing a list of 'RegObjStat' type objects to view.
                 return View(datasetRecords);
             }
             catch (Exception)
